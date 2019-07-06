c1adca3117c1fb185b07b5c13abcd49f4181045f
Daphne Lee <daphnelee92@gmail.com>
2019-07-06 15:37:50 -0400

    update state form

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
 
 export default class NewStudentForm extends Component {
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          First Name:
-          <input type="text" name="firstName" />
-        </label>
+    constructor() {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: ''
+        };
 
-        <label>
-          Last Name:
-          <input type="text" name="lastName" />
-        </label>
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+            //the brackets are computed property naming. Grabs the key
+        })
+    }
 
-        <button type="submit">Submit New Student</button>
-      </form>
-    );
-  }
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            firstName: '',
+            lastName: '',
+            email: ''
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    First Name:
+          <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    Last Name:
+          <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    Email:
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+                </label>
+
+                <button type="submit">Submit New Student</button>
+            </form>
+        );
+    }
 }
